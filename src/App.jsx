082b522238112573f8ab3376c853328b6dba2bff
Muhammad Umar <umr.m2305@gmail.com>
2025-04-28 01:32:20 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import Signup from "./Components/Signup/Signup";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Login from "./Components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Books from "./Components/Books/Books";
+import BorrowedBooks from "./Components/BorrowedBooks/BorrowedBooks";
+import ReturnedBooks from "./Components/ReturnedBooks/ReturnedBooks";
+
+function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div className="h-screen">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/borrowedbooks" element={<BorrowedBooks />} />
+        <Route path="/returnedbooks" element={<ReturnedBooks />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Tag/Tag.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Tag() {
+  return (
+    <div className="bg-white p-4 rounded-tl-lg rounded-bl-lg opacity-70 flex flex-col gap-4 fixed top-12 right-0">
+      <p className="">
+        © Muhammad Umar.
+        <br /> All rights reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Tag;
